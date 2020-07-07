5450804d5a77ad2420f63980a8d922c8211f3d03
Savostytskyi <savostytskyi.anton@gmail.com>
2020-07-07 14:30:39 +0200

    refactored to create common CrudInterface. Closes #11.

--- pet-clinic-data/src/main/java/com/aqa/asavostytskyi/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/aqa/asavostytskyi/springpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.aqa.asavostytskyi.springpetclinic.services;
 
 import com.aqa.asavostytskyi.springpetclinic.model.person.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
