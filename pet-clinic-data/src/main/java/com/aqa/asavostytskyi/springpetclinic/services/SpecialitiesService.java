b5290d18464b06f9acafe269600140676b6622f0
Savostytskyi <savostytskyi.anton@gmail.com>
2020-07-09 18:41:49 +0200

    added specialities Map Impl service. Closes #31

---
+++ pet-clinic-data/src/main/java/com/aqa/asavostytskyi/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,8 @@
+package com.aqa.asavostytskyi.springpetclinic.services;
+
+import com.aqa.asavostytskyi.springpetclinic.model.Specialty;
+
+public interface SpecialitiesService extends CrudService<Specialty, Long> {
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/aqa/asavostytskyi/springpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package com.aqa.asavostytskyi.springpetclinic.services.map;
+
+import com.aqa.asavostytskyi.springpetclinic.model.Specialty;
+import com.aqa.asavostytskyi.springpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public final class SpecialityServiceMap extends AbstractMapService<Specialty, Long>
+        implements SpecialitiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
