4a67339df10923d8ce6b80d52c7e90a31986dd0b
Savostytskyi <savostytskyi.anton@gmail.com>
2020-07-09 17:31:26 +0200

    added Specialty entity, associated with Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/com/aqa/asavostytskyi/springpetclinic/model/Specialty.java
@@ -0,0 +1,11 @@
+package com.aqa.asavostytskyi.springpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Specialty extends BaseEntity {
+
+    private String description;
+}
--- pet-clinic-data/src/main/java/com/aqa/asavostytskyi/springpetclinic/model/person/Vet.java
+++ pet-clinic-data/src/main/java/com/aqa/asavostytskyi/springpetclinic/model/person/Vet.java
@@ -1,4 +1,14 @@
 package com.aqa.asavostytskyi.springpetclinic.model.person;
 
+import com.aqa.asavostytskyi.springpetclinic.model.Specialty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
 }
